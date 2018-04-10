1f13075e820834c4bbc8cb03d0b9d10eecec124c
Stacey Vachon <stacey.vachon@ayogo.com>
2018-04-09 17:25:00 -0700

    2.6.1

--- dist/angular1.js
+++ dist/angular1.js
@@ -465,9 +465,11 @@ angular.module(modName, [])
             });
             mutationObserver.observe(el, { childList: true });
             $scope.$watch('index', function (newVal) {
-                var index = parseInt(newVal, 10);
-                carousel.setIndex(index);
-                carousel.snap(index);
+                if (newVal) {
+                    var index = parseInt(newVal, 10);
+                    carousel.setIndex(index);
+                    carousel.snap(index);
+                }
             });
             $scope.$on('$destroy', function () {
                 mutationObserver.disconnect();
